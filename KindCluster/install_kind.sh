d72611f398df4bc1b6fd14e9867024bc2dba20a8
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2025-08-16 19:09:32 +0200

    Deeplog updated

--- KindCluster/install_kind.sh
+++ KindCluster/install_kind.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-# Kind Installation
+# Kind Installation 
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.28.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
